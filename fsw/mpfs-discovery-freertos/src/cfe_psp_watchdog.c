0fdd8b44117b7035180d990d508a885659bac593
luisfranca <luis.franca@inf.ufrgs.br>
2025-10-09 09:56:43 -0300

    Implemented PSP Watchdog API

--- fsw/mpfs-discovery-freertos/src/cfe_psp_watchdog.c
+++ fsw/mpfs-discovery-freertos/src/cfe_psp_watchdog.c
@@ -7,7 +7,7 @@
 **   and usually don't fit well in the OS abstraction layer.
 **
 ** History:
-**   2025/05/20  França. Luís    | luis.franca@inf.ufrgs.br
+**   2025/05/20  Franca. Luís    | luis.franca@inf.ufrgs.br
 **
 *************************************************************************************************/
 
@@ -15,7 +15,13 @@
 #include "mpfs_hal/mss_hal.h"
 #include "drivers/mss/mss_watchdog/mss_watchdog.h"
 
-mss_watchdog_config_t wd0lo_config;
+uint32 CFE_PSP_WatchdogValue;
+
+void wdog0_tout_e51_local_IRQHandler_9(void)
+{
+    MSS_WD_clear_timeout_irq(MSS_WDOG0_LO);
+}
+
 
 /*----------------------------------------------------------------
  *
@@ -25,10 +31,20 @@ mss_watchdog_config_t wd0lo_config;
  *-----------------------------------------------------------------*/
 void CFE_PSP_WatchdogInit(void)
 {
-    PLIC_SetPriority(WDOG0_TOUT_PLIC, 2);
-    __enable_local_irq(WDOG0_MVRP_E51_INT);
+    mss_watchdog_config_t WatchdogConfig;
+
+    /* Disable Watchdog interruption before initialization. */
+    __disable_local_irq((int8_t)WDOG0_TOUT_E51_INT);
+
+    /* Reading the default config */
+    MSS_WD_get_config(MSS_WDOG0_LO, &WatchdogConfig);
 
 
+    WatchdogConfig.forbidden_en = MSS_WDOG_DISABLE;
+    WatchdogConfig.timeout_val = 0;
+    WatchdogConfig.time_val = MSS_WDOG_TIMER_MAX;
+
+    MSS_WD_configure(MSS_WDOG0_LO, &WatchdogConfig);
 }
 
 
@@ -40,7 +56,8 @@ void CFE_PSP_WatchdogInit(void)
  *-----------------------------------------------------------------*/
 void CFE_PSP_WatchdogEnable(void)
 {
-
+    MSS_WD_enable_mvrp_irq(MSS_WDOG0_LO);
+    __enable_local_irq((int8_t)WDOG0_TOUT_E51_INT);
 }
 
 /*----------------------------------------------------------------
@@ -51,7 +68,8 @@ void CFE_PSP_WatchdogEnable(void)
  *-----------------------------------------------------------------*/
 void CFE_PSP_WatchdogDisable(void)
 {
-
+    MSS_WD_disable_mvrp_irq(MSS_WDOG0_LO);
+    __disable_local_irq((int8_t)WDOG0_TOUT_E51_INT);
 }
 
 /*----------------------------------------------------------------
@@ -62,7 +80,7 @@ void CFE_PSP_WatchdogDisable(void)
  *-----------------------------------------------------------------*/
 void CFE_PSP_WatchdogService(void)
 {
-
+    MSS_WD_reload(MSS_WDOG0_LO);
 }
 
 /*----------------------------------------------------------------
@@ -73,7 +91,13 @@ void CFE_PSP_WatchdogService(void)
  *-----------------------------------------------------------------*/
 uint32 CFE_PSP_WatchdogGet(void)
 {
+    uint64_t WatchdogValueTicks;
+    uint64_t WatchdogValueMilliSecs;
 
+    WatchdogValueTicks = MSS_WD_current_value(MSS_WDOG0_LO);
+    WatchdogValueMilliSecs = WatchdogValueTicks;
+
+    return WatchdogValueMilliSecs;
 }
 
 /*----------------------------------------------------------------
@@ -84,5 +108,14 @@ uint32 CFE_PSP_WatchdogGet(void)
  *-----------------------------------------------------------------*/
 void CFE_PSP_WatchdogSet(uint32 WatchdogValue)
 {
-    // CFE_PSP_WatchdogValue = WatchdogValue;
+    uint64_t WatchdogValueTicks;
+    mss_watchdog_config_t WatchdogConfig;
+
+    WatchdogValueTicks = (WatchdogValue * (LIBERO_SETTING_MSS_SYSREG_CLKS_VERSION)) / 100;
+
+    MSS_WD_get_config(MSS_WDOG0_LO, &WatchdogConfig);
+
+    WatchdogConfig.time_val = WatchdogValueTicks;
+
+    MSS_WD_reload(MSS_WDOG0_LO);
 }
